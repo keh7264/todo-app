064454db440ca3cff2f7fadd9a84713c6b2a077f
eunhyeKim <keh7264@gmail.com>
2020-01-07 23:02:11 +0900

    Optimize performance

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트의 기초 알아보기', checked: true },
-    { id: 2, text: '컴포넌트 스타일링 해보기', checked: true },
-    { id: 3, text: '일정관리 앱 만들어 보기', checked: false },
-  ]);
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
 
-  const nextId = useRef(4);
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     text => {
@@ -20,7 +28,7 @@ function App() {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      setTodos(todos => todos.concat(todo));
       nextId.current += 1;
     },
     [todos],
@@ -28,14 +36,14 @@ function App() {
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos.filter(todo => todo.id !== id));
+      setTodos(todos => todos.filter(todo => todo.id !== id));
     },
     [todos],
   );
 
   const onToggle = useCallback(
     id => {
-      setTodos(
+      setTodos(todos =>
         todos.map(todo =>
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
@@ -43,6 +51,7 @@ function App() {
     },
     [todos],
   );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
